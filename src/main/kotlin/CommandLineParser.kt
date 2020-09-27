8428dd6d6500bc367714ecccfec7076389eda052
Sean Soper <sean.soper@gmail.com>
2020-09-27 17:24:43 -0400

    adding command line parser

---
+++ src/main/kotlin/CommandLineParser.kt
@@ -0,0 +1,67 @@
+package com.seansoper.batil
+
+import java.io.File
+import java.nio.file.Path
+import java.nio.file.Paths
+
+class CommandLineParser(private val args: Array<String>,
+                        private val basePath: String = System.getProperty("user.dir")) {
+
+    val shouldShowHelp: Boolean = args.any { it == "-help" }
+    val verbose: Boolean = args.any { it == "-verbose" }
+
+    data class Parsed(val pathToConfigFile: Path,
+                      val verbose: Boolean)
+
+    fun showHelp() {
+        val str = """
+            Arguments
+
+                -help              Show documentation
+                -verbose           Show debugging output
+                -config=path       Path to configuration file, default is ./batil.config
+        """.trimIndent()
+        println(str)
+    }
+
+    fun parse(): Parsed {
+        val pathToConfigFile = getPath("config") ?: Paths.get(basePath,"batil.config")
+
+        if (!File(pathToConfigFile.toString()).exists()) {
+            throw ConfigFileNotFound()
+        }
+
+        return Parsed(pathToConfigFile, verbose)
+    }
+
+    private fun<T: Any> parseArguments(regex: Regex, transform: (String) -> T): List<T> {
+        val match = fun (str: String): T? {
+            return regex.find(str)?.let {
+                if (it.groups.count() < 2) {
+                    return null
+                }
+
+                return it.groups[1]?.let {
+                    transform(it.value.removeSurrounding("\"").removeSurrounding("'"))
+                }
+            }
+        }
+
+        return args.mapNotNull(match)
+    }
+
+    private fun getPath(type: String): Path? {
+        val regex = Regex("^-${type}=(.*)")
+
+        return parseArguments(regex) {
+            if (it.startsWith("/")) {
+                Paths.get(it)
+            } else {
+                Paths.get(basePath, it)
+            }
+        }.firstOrNull()
+    }
+
+}
+
+class ConfigFileNotFound: Exception("Configuration file not found")
--- src/main/kotlin/Core.kt
+++ src/main/kotlin/Core.kt
@@ -1,7 +1,25 @@
 package com.seansoper.batil
 
+import kotlin.system.exitProcess
+
 object Core {
     @JvmStatic fun main(args: Array<String>) {
-        println("Hello World")
+        val cli = CommandLineParser(args)
+
+        if (cli.shouldShowHelp) {
+            cli.showHelp()
+            exitProcess(0)
+        }
+
+        val parsed = try {
+            cli.parse()
+        } catch (exception: ConfigFileNotFound) {
+            println("❌ ${exception.localizedMessage}")
+            cli.showHelp()
+            exitProcess(1)
+        }
+
+        println("verbose set to ${parsed.verbose}")
+        println("config path set to ${parsed.pathToConfigFile}")
     }
 }
