c8f15e4a40ef784cdf972098cfbfe76b5b49ba52
Sean Soper <sean.soper@gmail.com>
2020-10-02 18:17:11 -0400

    added etrade interceptor for use with okhttp, validated request token works

--- src/main/kotlin/Core.kt
+++ src/main/kotlin/Core.kt
@@ -1,6 +1,7 @@
 package com.seansoper.batil
 
 import okhttp3.OkHttpClient
+import okhttp3.Request
 import kotlin.system.exitProcess
 
 object Core {
@@ -32,7 +33,36 @@ object Core {
             exitProcess(1)
         }
 
-        val client = OkHttpClient()
-        
+        val keys = OauthKeys(
+                consumerKey = configuration.etrade.sandbox.key,
+                consumerSecret = configuration.etrade.sandbox.secret
+            )
+
+        val client = OkHttpClient.Builder()
+                .addInterceptor(EtradeInterceptor(keys))
+                .build()
+
+        val request = Request.Builder()
+                .url("https://apisb.etrade.com/oauth/request_token")
+                .build()
+
+        val response = client.newCall(request).execute()
+
+        if (parsed.verbose) {
+            println(request.headers)
+        }
+
+        val tokens = response.body?.string()?.
+            split("&").
+            takeIf { it?.isNotEmpty() ?: false }?.
+            map { it.split("=", limit = 2) }?.
+            filter {
+                it.size == 2 && it.first().contains("token")
+            }?.
+            also {
+                it.size == 2
+            }?.associate { it[0] to it[1] }
+
+        println(tokens)
     }
 }
