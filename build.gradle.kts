c4eeb94da343ddf90518aeabca3d063c75f22104
Sean Soper <sean.soper@gmail.com>
2021-08-31 21:37:20 -0400

    upgraded jackson deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,9 +10,9 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.5.0")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.11.2")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.11.2")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.11.2")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.4")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.12.4")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.12.4")
     implementation("com.squareup.okhttp3:okhttp:4.9.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.9.0")
     implementation("pl.wendigo:chrome-reactive-kotlin:0.6.1")
