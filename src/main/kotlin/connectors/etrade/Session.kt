c319b52742edd0c6b4c9730accda3d2f577cc004
ssoper <sean.soper@gmail.com>
2021-09-13 22:25:27 +0000

    Deploying to gh-pages from @ ssoper/Batil@67c1fc65938010eb24cc7ee84fed4ec70f0a85d2 🚀

--- src/main/kotlin/connectors/etrade/Session.kt
+++
@@ -1,7 +0,0 @@
-package com.seansoper.batil.connectors.etrade
-
-class Session(val consumerKey: String,
-              val consumerSecret: String,
-              val accessToken: String,
-              val accessSecret: String,
-              val verifier: String)
