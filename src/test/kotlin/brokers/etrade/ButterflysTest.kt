45eeade23c63de4401342c58bf6f9218dfc4aadc
Sean Soper <sean.soper@gmail.com>
2021-09-28 09:03:30 -0400

    add support for selling butterfly calls/puts

--- src/test/kotlin/brokers/etrade/ButterflysTest.kt
+++ src/test/kotlin/brokers/etrade/ButterflysTest.kt
@@ -128,4 +128,104 @@ class ButterflysTest : StringSpec({
             it.takeRequest().path.shouldBe("/v1/accounts/$accountIdKey/orders/preview")
         }
     }
+
+    "create preview to buy butterfly puts" {
+        val path = Paths.get("brokers/etrade/orders/createPreview/butterflys/buy_puts.json")
+
+        createServer(path) {
+            val limitPrice = 0.19f
+            val strikes = listOf(18f, 19f, 20f)
+            val service = Orders(mockSession(), baseUrl = it.url(".").toString())
+            val request = buyButterflyCalls(
+                chargePoint.symbol,
+                Triple(strikes[0], strikes[1], strikes[2]),
+                limitPrice,
+                quantity
+            )
+            val data = service.createPreview(accountIdKey, request)
+
+            data.shouldNotBeNull()
+            data.orderType.shouldBe(OrderType.BUTTERFLY)
+
+            val order = data.orders.first()
+            order.messages.shouldBeNull()
+            order.orderTerm.shouldBe(OrderTerm.GOOD_FOR_DAY)
+            order.priceType.shouldBe(OrderPriceType.NET_DEBIT)
+            order.limitPrice.shouldBe(limitPrice)
+
+            order.instruments!!.forEachIndexed { index, instrument ->
+                instrument.product!!.let {
+                    it.symbol.shouldBe(chargePoint.symbol)
+                    it.expiryYear.shouldBe(chargePoint.year)
+                    it.expiryMonth.shouldBe(chargePoint.month)
+                    it.expiryDay.shouldBe(chargePoint.day)
+                    it.callPut.shouldBe(OptionType.PUT)
+                    it.strikePrice.shouldBe(strikes[index])
+                }
+
+                when (index) {
+                    0, 2 -> {
+                        instrument.orderAction.shouldBe(OrderActionType.BUY_OPEN)
+                        instrument.quantity.shouldBe(quantity.toFloat())
+                    }
+                    else -> {
+                        instrument.orderAction.shouldBe(OrderActionType.SELL_OPEN)
+                        instrument.quantity.shouldBe(quantity.toFloat() * 2)
+                    }
+                }
+            }
+
+            it.takeRequest().path.shouldBe("/v1/accounts/$accountIdKey/orders/preview")
+        }
+    }
+
+    "create preview to sell butterfly puts" {
+        val path = Paths.get("brokers/etrade/orders/createPreview/butterflys/sell_puts.json")
+
+        createServer(path) {
+            val limitPrice = 0.19f
+            val strikes = listOf(18f, 19f, 20f)
+            val service = Orders(mockSession(), baseUrl = it.url(".").toString())
+            val request = sellButterflyCalls(
+                chargePoint.symbol,
+                Triple(strikes[0], strikes[1], strikes[2]),
+                limitPrice,
+                quantity
+            )
+            val data = service.createPreview(accountIdKey, request)
+
+            data.shouldNotBeNull()
+            data.orderType.shouldBe(OrderType.BUTTERFLY)
+
+            val order = data.orders.first()
+            order.messages.shouldBeNull()
+            order.orderTerm.shouldBe(OrderTerm.GOOD_FOR_DAY)
+            order.priceType.shouldBe(OrderPriceType.NET_CREDIT)
+            order.limitPrice.shouldBe(limitPrice)
+
+            order.instruments!!.forEachIndexed { index, instrument ->
+                instrument.product!!.let {
+                    it.symbol.shouldBe(chargePoint.symbol)
+                    it.expiryYear.shouldBe(chargePoint.year)
+                    it.expiryMonth.shouldBe(chargePoint.month)
+                    it.expiryDay.shouldBe(chargePoint.day)
+                    it.callPut.shouldBe(OptionType.PUT)
+                    it.strikePrice.shouldBe(strikes[index])
+                }
+
+                when (index) {
+                    0, 2 -> {
+                        instrument.orderAction.shouldBe(OrderActionType.SELL_OPEN)
+                        instrument.quantity.shouldBe(quantity.toFloat())
+                    }
+                    else -> {
+                        instrument.orderAction.shouldBe(OrderActionType.BUY_OPEN)
+                        instrument.quantity.shouldBe(quantity.toFloat() * 2)
+                    }
+                }
+            }
+
+            it.takeRequest().path.shouldBe("/v1/accounts/$accountIdKey/orders/preview")
+        }
+    }
 })
